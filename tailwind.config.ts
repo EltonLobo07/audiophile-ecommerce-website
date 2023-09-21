5e78907d213b74f1f4f8884e03760f8ab234dbd5
Elton Lobo <eltonsw00@gmail.com>
2023-09-21 13:13:14 +0530

    Initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,38 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontFamily: {
+      sans: ["var(--font-manrope)"]
+    },
+    screens: {
+      tabAndUp: "45rem", // 720px 
+      laptopAndUp: "68.75rem" // 1100px
+    },
     extend: {
+      borderRadius: {
+        "2": "0.5rem",
+        "inherit": "inherit"
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        // Format
+        // <COLOR_NAME_ASSIGNED_BY_ME>: <COLOR_HEX_VALUE>[,] // <OFFICIAL_COLOR_NAME>
+        "dark-orange": "#D87D4A", // seraphim sepia
+        "chaos-black": "#101010", // chaos black
+        "grayish-white": "#F1F1F1", // beluga
+        "almost-white": "#FAFAFA", // dr. white
+        "black-suede": "#434343", // black suede
+        "light-orange": "#FBAF85", // slice pink
+        "thamar-black": "#191919", // thamar black
+        "white": "#FFFFFF", // white
+        "black": "#000000", // black
+        "argent": "#888888",  // argent
+        "shady-character": "#4C4C4C" // shady character
+      }
     },
   },
   plugins: [],
